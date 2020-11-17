85ce7670643b5b76a98fe130ab795416c24dd4d6
dahkimr <soobakyojin57@hotmail.com>
2020-11-17 12:04:05 -0500

    commit before merge

--- src/log_reg.py
+++ src/log_reg.py
@@ -4,39 +4,14 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.feature_extraction import text
 from sklearn.linear_model import LogisticRegression
 
+import gensim.downloader as api
+
 # References
 # https://analyticsindiamag.com/hands-on-guide-to-predict-fake-news-using-logistic-regression-svm-and-naive-bayes-methods/
 
 def main(folder_path, train_file, test_file, save_file):
 
-    test = [
-        [
-            ["This is a sentence"],
-            ["The sky is blue and white"],
-            ["I don't want to study"]
-        ],
-        [
-            ["Can dogs see colours"],
-            ["What goes on in cat's minds"]
-        ],
-        [
-            ["Spaghetti is made by boiling noodles"],
-            ["Use fresh tomatoes for better flavour"],
-            ["Don't forget the garlic bread"]
-        ]
-    ]
-
-    test2 = [
-         "This is a sentence. The sky is blue and white. I don't want to study",
-        "Can dogs see colours? What goes on in cat's minds",
-        "Spaghetti is made by boiling noodles. Use fresh tomatoes for better flavour. Don't forget the garlic bread"
-    ]
-
-    vectorizer = TfidfVectorizer(ngram_range=(1, 2))
-    X = vectorizer.fit_transform(test2)
-    print(vectorizer.get_feature_names())
-
-    # train_X, train_y = util.load_dataset(folder_path, train_file)
+    train_X, train_y = util.load_dataset(folder_path, train_file)
     # test_X, test_y = util.load_dataset(folder_path, test_file)
 
     # lrm = LogisticRegressionModel()
@@ -44,6 +19,11 @@ def main(folder_path, train_file, test_file, save_file):
     # pred_y = lrm.predict(test_X)
     #
     # util.print_accuracy_measures(test_y, pred_y)
+
+    info = api.info()
+    # this takes a lot of time
+    # maybe create script that loads and makes for input file?
+    word_model = api.load("word2vec-google-news-300")
 
 class LogisticRegressionModel:
     """
