bc174910e7ef03ad4558da6b2898c983e5767167
Christian Wang <christian.gnaw@gmail.com>
2020-12-12 18:36:30 -0500

    LOL

--- src/models/log_reg/log_reg_comp_unigram.py
+++ src/models/log_reg/log_reg_comp_unigram.py
@@ -1,7 +1,7 @@
 # To add a new cell, type '# %%'
 # To add a new markdown cell, type '# %% [markdown]'
 # %% [markdown]
-# <h1> Naive Bayes Model on Kaggle Competition Fake News Dataset </h1>
+# <h1> Logistic Regression Model on Kaggle Competition Fake News Dataset </h1>
 # <h3> The fake news dataset is publicly available <a href="https://www.kaggle.com/c/fake-news">here</a></h3>
 
 # %%
@@ -29,24 +29,24 @@ base_model.fit(train_X, train_Y)
 
 # %%
 pred_Y = base_model.predict(test_X)
-util.print_accuracy_measures(test_Y, pred_Y, label="naive_bayes_uni_base_comp")
+util.print_accuracy_measures(test_Y, pred_Y, label="log_reg_uni_base_comp")
 
 
 # %%
-util.visualize_confusion_matrix(base_model,test_X, test_Y,"naive_bayes_uni_base_comp_confusion_matrix")
+util.visualize_confusion_matrix(base_model,test_X, test_Y,"log_reg_uni_base_comp_confusion_matrix")
 
 
 # %%
 base_cv_results = cross_validate(base_model, train_X, train_Y, cv=KFold(5))
-util.plot_cv_score(base_cv_results,title="naive_bayes_uni_base_comp_cv_score_bar")
+util.plot_cv_score(base_cv_results,title="log_reg_uni_base_comp_cv_score_bar")
 
 
 # %%
-util.plot_learning_curve(base_model, "naive_bayes_uni_base_comp_learning_curve", train_X,train_Y, cv=KFold(5), n_jobs=4)
+util.plot_learning_curve(base_model, "log_reg_uni_base_comp_learning_curve", train_X,train_Y, cv=KFold(5), n_jobs=4)
 
 
 # %%
-util.plot_word_cloud(base_model,"naive_bayes_uni_base_comp_word_cloud")
+util.plot_word_cloud(base_model,"log_reg_uni_base_comp_word_cloud")
 
 # %% [markdown]
 # <h2> Adding TFIDF </h2>
@@ -58,23 +58,23 @@ tfidf_model.fit(train_X, train_Y)
 
 # %%
 pred_Y = tfidf_model.predict(test_X)
-util.print_accuracy_measures(test_Y, pred_Y, label="naive_bayes_uni_tfidf_comp")
+util.print_accuracy_measures(test_Y, pred_Y, label="log_reg_uni_tfidf_comp")
 
 
 # %%
-util.visualize_confusion_matrix(base_model,test_X, test_Y,"naive_bayes_uni_tfidf_comp_confusion_matrix")
+util.visualize_confusion_matrix(base_model,test_X, test_Y,"log_reg_uni_tfidf_comp_confusion_matrix")
 
 
 # %%
 tfidf_cv_results = cross_validate(base_model, train_X, train_Y, cv=KFold(5))
-util.plot_cv_score(base_cv_results,title="naive_bayes_uni_tfidf_comp_cv_score_bar")
+util.plot_cv_score(base_cv_results,title="log_reg_uni_tfidf_comp_cv_score_bar")
 
 
 # %%
-util.plot_learning_curve(base_model, "naive_bayes_uni_tfidf_comp_learning_curve", train_X,train_Y, cv=KFold(5), n_jobs=4)
+util.plot_learning_curve(base_model, "log_reg_uni_tfidf_comp_learning_curve", train_X,train_Y, cv=KFold(5), n_jobs=4)
 
 
 # %%
-util.plot_word_cloud(tfidf_model,"naive_bayes_uni_tfidf_comp_word_cloud")
+util.plot_word_cloud(tfidf_model,"log_reg_uni_tfidf_comp_word_cloud")
 
 
---
+++ src/models/log_reg/py_run_batch.bat
@@ -0,0 +1,4 @@
+python "C:\Users\Chris\Documents\Projects\CPS803-Project\src\models\log_reg\log_reg_clement_unigram.py"
+python "C:\Users\Chris\Documents\Projects\CPS803-Project\src\models\log_reg\log_reg_clement_bigram.py"
+python "C:\Users\Chris\Documents\Projects\CPS803-Project\src\models\log_reg\log_reg_comp_unigram.py"
+python "C:\Users\Chris\Documents\Projects\CPS803-Project\src\models\log_reg\log_reg_comp_bigram.py"
