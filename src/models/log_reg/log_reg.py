d4d51c726a83efc59d0b10b28b08fb6c224bccf9
Christian Wang <christian.gnaw@gmail.com>
2020-12-12 16:51:53 -0500

    Added log_reg

--- src/models/log_reg/log_reg.py
+++ src/models/log_reg/log_reg.py
@@ -1,9 +1,15 @@
 from sklearn.pipeline import Pipeline
-from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
 from sklearn.feature_extraction import text
 from sklearn.linear_model import LogisticRegression
 
-def LogRegModel(ngram=(1, 1)):
+def BaseLogRegModel(ngram=(1, 1)):
+    return Pipeline([
+        ('tfidf', CountVectorizer(stop_words=text.ENGLISH_STOP_WORDS, ngram_range=ngram)),
+        ('model', LogisticRegression())
+    ])
+
+def TFIDFLogRegModel(ngram=(1, 1)):
     return Pipeline([
         ('tfidf', TfidfVectorizer(stop_words=text.ENGLISH_STOP_WORDS, ngram_range=ngram)),
         ('model', LogisticRegression())
