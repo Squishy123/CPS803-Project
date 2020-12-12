1e0a41bfdaafb36b0d29acac5858ee32e2105fbf
Christian Wang <christian.gnaw@gmail.com>
2020-12-12 16:52:55 -0500

    LOL

--- src/models/log_reg/log_reg.py
+++ src/models/log_reg/log_reg.py
@@ -5,12 +5,12 @@ from sklearn.linear_model import LogisticRegression
 
 def BaseLogRegModel(ngram=(1, 1)):
     return Pipeline([
-        ('tfidf', CountVectorizer(stop_words=text.ENGLISH_STOP_WORDS, ngram_range=ngram)),
+        ('features', CountVectorizer(stop_words=text.ENGLISH_STOP_WORDS, ngram_range=ngram)),
         ('model', LogisticRegression())
     ])
 
 def TFIDFLogRegModel(ngram=(1, 1)):
     return Pipeline([
-        ('tfidf', TfidfVectorizer(stop_words=text.ENGLISH_STOP_WORDS, ngram_range=ngram)),
+        ('features', TfidfVectorizer(stop_words=text.ENGLISH_STOP_WORDS, ngram_range=ngram)),
         ('model', LogisticRegression())
     ])
