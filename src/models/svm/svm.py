a54dd16bea89d875766f4c98bd55364c18694d27
Christian Wang <christian.gnaw@gmail.com>
2020-12-13 13:28:53 -0500

    added svm plots and stuff

--- src/models/svm/svm.py
+++ src/models/svm/svm.py
@@ -5,10 +5,10 @@ from sklearn import svm
 
 def BaseSVMModel(ngram=(1, 1)):
     return Pipeline([
-        ('features', CountVectorizer(stop_words=text.ENGLISH_STOP_WORDS, ngram_range=ngram)),
+        ('features', CountVectorizer(max_features=1000, stop_words=text.ENGLISH_STOP_WORDS, ngram_range=ngram)),
         ('model', svm.SVC(kernel="linear", C=0.025))])
 
 def TFIDFSVMModel(ngram=(1, 1)):
     return Pipeline([
-        ('features', TfidfVectorizer(stop_words=text.ENGLISH_STOP_WORDS, ngram_range=ngram)),
+        ('features', TfidfVectorizer(max_features=1000, stop_words=text.ENGLISH_STOP_WORDS, ngram_range=ngram)),
         ('model', svm.SVC(kernel="linear", C=0.025))])
