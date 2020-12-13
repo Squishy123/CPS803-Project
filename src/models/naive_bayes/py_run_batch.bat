c69865a8bb4e065d8476941527ac4d30f0ac7033
Christian Wang <christian.gnaw@gmail.com>
2020-12-12 20:14:53 -0500

    script for naive_bayes

---
+++ src/models/naive_bayes/py_run_batch.bat
@@ -0,0 +1,4 @@
+python "C:\Users\chris\Documents\School\CPS803-Project\src\models\naive_bayes\naive_bayes_clement_unigram.py"
+python "C:\Users\chris\Documents\School\CPS803-Project\src\models\naive_bayes\naive_bayes_clement_bigram.py"
+python "C:\Users\chris\Documents\School\CPS803-Project\src\models\naive_bayes\naive_bayes_comp_unigram.py"
+python "C:\Users\chris\Documents\School\CPS803-Project\src\models\naive_bayes\naive_bayes_comp_bigram.py"
